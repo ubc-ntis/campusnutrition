283f4448dbe1e7b0b24566e105fe1874e36991bd
kaskamal <kasra.kamal@gmail.com>
2019-11-03 14:10:39 -0800

    Revert changes to actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -27,7 +27,7 @@ const stopLoading = () => ({
 export const loadRestaurants = () => (dispatch) => {
     dispatch(startLoading());
 
-    fetch()
+    fetch("https://mate-uber-eats-api.herokuapp.com/api/v1/restaurants")
         .then(res => res.json())
         .then(({ data }) => {
             dispatch(saveRestaurants(data));
