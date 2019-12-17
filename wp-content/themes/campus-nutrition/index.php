c27179dce44d0dd7d759add4501a2e6e3435ed00
DawtDawt <tjwu24@gmail.com>
2019-12-16 21:10:53 -0800

    /themes: still learning

--- wp-content/themes/campus-nutrition/index.php
+++ wp-content/themes/campus-nutrition/index.php
@@ -1,1 +1,19 @@
-INDEX TODO
+<?php
+    function testAddOne($value) {
+        echo 1 + $value;
+    }
+?>
+
+<h1>
+    <?php 
+        bloginfo('name');
+    ?>
+</h1>
+
+<p>
+    <?php 
+        bloginfo('description');
+        echo nl2br("\n");
+        testAddOne(4);
+    ?>
+</p>
