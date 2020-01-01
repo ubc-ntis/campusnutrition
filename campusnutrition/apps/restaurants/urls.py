c2b341971bef64edbdf488908dc49d63dad06169
kaskamal <kasra.kamal@gmail.com>
2019-12-31 22:42:38 -0800

    Add redirection to ubc restaurants

--- campusnutrition/apps/restaurants/urls.py
+++ campusnutrition/apps/restaurants/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:rest_name>/', views.restaurant, name='restaurant'),
+    path('', views.redirect_view),
+    path('<str:area>/', views.home, name='index'),
 ]
--- campusnutrition/apps/restaurants/views.py
+++ campusnutrition/apps/restaurants/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 
 from .models import Restaurant
 
 # Create your views here.
 
-def index(request):
-    restaurant_list = Restaurant.objects.all()
+def redirect_view(request):
+    response = redirect("/ubc/")
+    return response
+
+def home(request, area):
+    restaurant_list = Restaurant.objects.filter(area=area)
     context = {
         'restaurant_list': restaurant_list
     }
-    return render(request, 'restaurants/index.html', context)
+    return render(request, 'restaurants/home.html', context)
 
-
-def restaurant(request, rest_name):
-    restaurant = get_object_or_404(Restaurant, name=rest_name)
-    return HttpResponse("You're viewing restaurant %s" % rest_name)
