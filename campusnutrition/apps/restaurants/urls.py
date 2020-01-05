ea11757b1aaa79f3d0d658d3351c2c8bf55eb4bd
kaskamal <kasra.kamal@gmail.com>
2020-01-04 16:18:10 -0800

    Add contact and about paths

--- campusnutrition/apps/restaurants/urls.py
+++ campusnutrition/apps/restaurants/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.redirect_view),
-    path('<str:area>/', views.home, name='index'),
+    path('',           views.redirect_view),
+    path('about/',      views.about,   name='about'),
+    path('contact/',    views.contact, name='contact'),
+    path('<str:area>/', views.home,    name='home'),
 ]
