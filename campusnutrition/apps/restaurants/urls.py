214e67304c3fb7773379505f47805ae55106c9aa
kaskamal <kasra.kamal@gmail.com>
2020-01-05 14:02:28 -0800

    Change map endpoint

--- campusnutrition/apps/restaurants/urls.py
+++ campusnutrition/apps/restaurants/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',           views.redirect_view),
-    path('about/',      views.about,   name='about'),
-    path('contact/',    views.contact, name='contact'),
-    path('<str:area>/', views.home,    name='home'),
+    path('',                views.redirect_view),
+    path('about/',          views.about,   name='about'),
+    path('contact/',        views.contact, name='contact'),
+    path('<str:area>/map/', views.map,     name='map'),
+    path('<str:area>/',     views.home,    name='home'),
 ]
